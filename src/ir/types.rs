ff2856b6586d063ec203165d047a52e0a9ef1fdc
MaxXSoft <x@MaxXSoft.net>
2021-08-16 16:59:58 +0800

    (types) removed 'TypeFactory'

--- src/ir/types.rs
+++ src/ir/types.rs
@@ -1,5 +1,7 @@
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::rc::Rc;
+use std::sync::Mutex;
 use std::{cmp, convert, fmt};
 
 /// Type data.
@@ -43,6 +45,47 @@ impl fmt::Display for TypeData {
 #[derive(Hash, Clone, Eq)]
 pub struct Type(Rc<TypeData>);
 
+lazy_static! {
+  /// Pool of all created types.
+  static ref TYPES: Mutex<HashMap<TypeData, Type>> = Mutex::new(HashMap::new());
+}
+
+impl Type {
+  /// Gets a specific type.
+  pub fn get_type(type_data: TypeData) -> Type {
+    TYPES
+      .lock()
+      .unwrap()
+      .get(&type_data)
+      .cloned()
+      .unwrap_or_else(|| {
+        let k = Type(Rc::new(type_data.clone()));
+        TYPES.lock().unwrap().insert(type_data, k.clone());
+        k
+      })
+  }
+
+  /// Gets an `i32` type.
+  pub fn get_i32() -> Type {
+    Type::get_type(TypeData::Int32)
+  }
+
+  /// Gets an array type.
+  pub fn get_array(base: Type, len: usize) -> Type {
+    Type::get_type(TypeData::Array(base, len))
+  }
+
+  /// Gets an pointer type.
+  pub fn get_pointer(base: Type) -> Type {
+    Type::get_type(TypeData::Pointer(base))
+  }
+
+  /// Gets an function type.
+  pub fn get_function(ret: Option<Type>, args: Vec<Type>) -> Type {
+    Type::get_type(TypeData::Function(ret, args))
+  }
+}
+
 impl cmp::PartialEq for Type {
   fn eq(&self, other: &Self) -> bool {
     Rc::ptr_eq(&self.0, &other.0)
@@ -58,47 +101,5 @@ impl convert::AsRef<TypeData> for Type {
 impl fmt::Display for Type {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "{}", self.0)
-  }
-}
-
-/// Type factory for creating types.
-pub struct TypeFactory {
-  types: HashMap<TypeData, Type>,
-}
-
-impl TypeFactory {
-  pub fn new() -> Self {
-    TypeFactory {
-      types: HashMap::new(),
-    }
-  }
-
-  /// Gets a specific type.
-  pub fn get_type(&mut self, type_data: TypeData) -> Type {
-    self.types.get(&type_data).cloned().unwrap_or_else(|| {
-      let k = Type(Rc::new(type_data.clone()));
-      self.types.insert(type_data, k.clone());
-      k
-    })
-  }
-
-  /// Gets an `i32` type.
-  pub fn get_i32(&mut self) -> Type {
-    self.get_type(TypeData::Int32)
-  }
-
-  /// Gets an array type.
-  pub fn get_array(&mut self, base: Type, len: usize) -> Type {
-    self.get_type(TypeData::Array(base, len))
-  }
-
-  /// Gets an pointer type.
-  pub fn get_pointer(&mut self, base: Type) -> Type {
-    self.get_type(TypeData::Pointer(base))
-  }
-
-  /// Gets an function type.
-  pub fn get_function(&mut self, ret: Option<Type>, args: Vec<Type>) -> Type {
-    self.get_type(TypeData::Function(ret, args))
   }
 }
