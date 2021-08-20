6ae4ea2ceda1f12086607a081283d8216fa1591e
MaxXSoft <x@MaxXSoft.net>
2021-08-20 13:22:55 +0800

    (values) updated some details

--- src/ir/values.rs
+++ src/ir/values.rs
@@ -1,77 +1,56 @@
-use crate::ir::core::{Use, Value, ValueData};
+use crate::ir::core::{unwrap_kind, Use, Value, ValueKind, ValueRc};
 use crate::ir::types::Type;
-use crate::ir::{Node, NodeRc};
-use crate::{impl_user, impl_value};
-use std::cell::RefCell;
 use std::rc::Rc;
 
 /// Integer constant.
 pub struct Integer {
-  data: ValueData,
   value: i32,
 }
 
-impl_value!(Integer, data);
-
 impl Integer {
-  // TODO: pool
-  pub fn new(value: i32) -> NodeRc {
-    Rc::new(RefCell::new(Node::Integer(Integer {
-      data: ValueData::new(Type::get_i32()),
-      value: value,
-    })))
+  todo!("pool");
+  pub fn new(value: i32) -> ValueRc {
+    Value::new(
+      Type::get_i32(),
+      ValueKind::Integer(Integer { value: value }),
+    )
   }
 
+  /// Gets the integer value.
   pub fn value(&self) -> i32 {
     self.value
   }
 }
 
 /// Zero initializer.
-pub struct ZeroInit {
-  data: ValueData,
-}
-
-impl_value!(ZeroInit, data);
+pub struct ZeroInit;
 
 impl ZeroInit {
-  // TODO: pool
-  pub fn new(ty: Type) -> NodeRc {
-    Rc::new(RefCell::new(Node::ZeroInit(ZeroInit {
-      data: ValueData::new(ty),
-    })))
+  todo!("pool");
+  pub fn new(ty: Type) -> ValueRc {
+    Value::new(ty, ValueKind::ZeroInit(ZeroInit))
   }
 }
 
 /// Undefined value.
-pub struct Undef {
-  data: ValueData,
-}
-
-impl_value!(Undef, data);
+pub struct Undef;
 
 impl Undef {
-  // TODO: pool
-  pub fn new(ty: Type) -> NodeRc {
-    Rc::new(RefCell::new(Node::Undef(Undef {
-      data: ValueData::new(ty),
-    })))
+  todo!("pool");
+  pub fn new(ty: Type) -> ValueRc {
+    Value::new(ty, ValueKind::Undef(Undef))
   }
 }
 
 /// Aggregate value.
 ///
-/// This 'value' is actually an `User`.
+/// This 'value' is actually an user.
 pub struct Aggregate {
-  data: ValueData,
   elems: Vec<Rc<Use>>,
 }
 
-impl_value!(Aggregate, data);
-impl_user!(Aggregate, elems);
-
 impl Aggregate {
-  pub fn new(elems: Vec<NodeRc>) -> NodeRc {
+  pub fn new(elems: Vec<ValueRc>) -> ValueRc {
     // element list should not be empty
     debug_assert!(!elems.is_empty(), "`elem` is empty!");
     // check if all elements have the same type
@@ -81,18 +60,13 @@ impl Aggregate {
         .all(|e| e[0].borrow().ty() == e[1].borrow().ty()),
       "type mismatch in `elem`!"
     );
-    // create node
-    let node = Rc::new(RefCell::new(Node::Aggregate(Aggregate {
-      data: ValueData::new(*elems[0].borrow().ty()),
-      elems: vec![],
-    })));
-    // initialize element list
-    let user = Rc::downgrade(&node);
-    let elems = elems.into_iter().map(|e| Use::new(e, user)).collect();
-    match &mut *node.borrow_mut() {
-      Node::Aggregate(agg) => agg.elems = elems,
-      _ => unreachable!(),
-    };
-    node
+    // create value
+    Value::new_with_init(
+      *elems[0].borrow().ty(),
+      ValueKind::Aggregate(Aggregate { elems: vec![] }),
+      |user, kind| {
+        unwrap_kind!(kind, Aggregate).elems = elems.into_iter().map(|e| Use::new(e, user)).collect()
+      },
+    )
   }
 }
