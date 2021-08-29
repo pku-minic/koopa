458ff12cc6744166abbdfe4dcb136aa506f54f2c
MaxXSoft <x@MaxXSoft.net>
2021-08-29 09:24:18 +0800

    (ast) added more implementations

--- src/front/ast.rs
+++ src/front/ast.rs
@@ -1,4 +1,5 @@
 use crate::front::span::Span;
+use crate::ir::instructions::{BinaryOp, UnaryOp};
 
 /// AST of Koopa IR.
 pub struct Ast {
@@ -10,11 +11,95 @@ pub struct Ast {
 pub type AstBox = Box<Ast>;
 
 impl Ast {
-  // TODO
+  fn new(span: Span, kind: AstKind) -> Self {
+    Self { span, kind }
+  }
 }
 
 /// Kind of AST.
 pub enum AstKind {
+  /// 32-bit integer type.
+  IntType,
+  /// Array type.
+  ArrayType { base: AstBox, len: usize },
+  /// Pointer type.
+  PointerType { base: AstBox },
+  /// Function type.
+  FunType { params: Vec<AstBox>, ret: AstBox },
+
+  /// Symbol refernce.
+  SymbolRef { symbol: String },
+  /// Integer literal.
+  IntVal { value: i64 },
+  /// Undefined value.
+  UndefVal,
+  /// Aggregate value.
+  Aggregate { elems: Vec<AstBox> },
+  /// Zero initializer.
+  ZeroInit,
+
+  /// Symbol definition.
+  SymbolDef { name: String, value: AstBox },
+  /// Global symbol definition.
+  GlobalDef { name: String, value: AstBox },
+
+  /// Memory declaration.
+  MemDecl { ty: AstBox },
+  /// Global memory declaration.
+  GlobalDecl { ty: AstBox, init: AstBox },
+  /// Load.
+  Load { symbol: String },
+  /// Store.
+  Store { value: AstBox, symbol: String },
+  /// Pointer calculation.
+  GetPointer {
+    symbol: String,
+    value: AstBox,
+    step: Option<i32>,
+  },
+
+  /// Binary expression.
+  BinaryExpr {
+    op: BinaryOp,
+    lhs: AstBox,
+    rhs: AstBox,
+  },
+  /// Unary expression.
+  UnaryExpr { op: UnaryOp, opr: AstBox },
+
+  /// Branch.
+  Branch {
+    cond: AstBox,
+    tbb: String,
+    fbb: String,
+  },
+  /// Jump.
+  Jump { target: String },
+
+  /// Function call.
+  FunCall { fun: String, args: Vec<AstBox> },
+  /// Return.
+  Return { value: Option<AstBox> },
+
+  /// Function definition.
+  FunDef {
+    name: String,
+    params: Vec<(String, AstBox)>,
+    ret: Option<AstBox>,
+    bbs: Vec<AstBox>,
+  },
+  /// Basic block.
+  Block { name: String, stmts: Vec<AstBox> },
+  /// Function declaration.
+  FunDecl {
+    name: String,
+    params: Vec<AstBox>,
+    ret: Option<AstBox>,
+  },
+
+  /// Phi function.
+  Phi { oprs: Vec<(AstBox, String)> },
+
   /// End of file.
   End,
 }
