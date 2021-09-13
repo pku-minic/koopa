f2ddbc2130881029b7bd5652c614734d149fb49e
MaxXSoft <x@MaxXSoft.net>
2021-09-13 19:06:56 +0800

    (mod) added type alias 'KoopaGenerator'

--- src/back/mod.rs
+++ src/back/mod.rs
@@ -1,2 +1,7 @@
 pub mod generator;
 pub mod koopa;
+
+use generator::Generator;
+
+/// Generator for generating Koopa IR structures into text formatted Koopa IR.
+pub type KoopaGenerator<W> = Generator<W, koopa::Visitor>;
