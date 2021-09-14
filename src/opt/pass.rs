60943d3cabe8bfc7cb8b2be211cbe781244a177e
MaxXSoft <x@MaxXSoft.net>
2021-09-14 19:32:11 +0800

    (pass) added module 'opt::pass'

---
+++ src/opt/pass.rs
@@ -0,0 +1,26 @@
+use crate::ir::structs::{BasicBlock, Function, Program};
+
+/// A Koopa IR pass.
+pub enum Pass {
+  Module(Box<dyn ModulePass>),
+  Function(Box<dyn FunctionPass>),
+  BasicBlock(Box<dyn BasicBlockPass>),
+}
+
+/// Module pass, runs on IR programs.
+pub trait ModulePass {
+  /// Runs on the specific IR program.
+  fn run_on(&mut self, program: &Program);
+}
+
+/// Function pass, runs on functions.
+pub trait FunctionPass {
+  /// Runs on the specific Function.
+  fn run_on(&mut self, func: &Function);
+}
+
+/// Basic block pass, runs on basic blocks.
+pub trait BasicBlockPass {
+  /// Runs on the specific basic block.
+  fn run_on(&mut self, bb: &BasicBlock);
+}
