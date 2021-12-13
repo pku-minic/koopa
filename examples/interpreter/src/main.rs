7c7d580fa8d4f09a82f5aa8074067cbec31d981f
MaxXSoft <x@MaxXSoft.net>
2021-12-13 12:30:14 +0800

    (main) modified some details

--- examples/interpreter/src/main.rs
+++ examples/interpreter/src/main.rs
@@ -2,6 +2,7 @@ mod ext_funcs;
 
 use ext_funcs::ExternFuncs;
 use koopa::back::{Generator, NameManager, Visitor};
+use koopa::ir::instructions::*;
 use koopa::ir::{BasicBlock, Function, Program, Type, TypeKind, Value, ValueKind};
 use koopa::value;
 use std::collections::HashMap;
@@ -14,7 +15,8 @@ fn main() {
 }
 
 struct Interpreter {
-  vars: HashMap<*const Value, Box<Val>>,
+  global_allocs: Vec<Box<Val>>,
+  vars: HashMap<*const Value, Val>,
   envs: Vec<Environment>,
   ext_funcs: ExternFuncs,
 }
@@ -28,7 +30,10 @@ impl<W: Write> Visitor<W> for Interpreter {
       match var.kind() {
         ValueKind::GlobalAlloc(ga) => {
           let val = Self::eval_const(value!(ga.init()))?;
-          self.vars.insert(var, Box::new(val));
+          self.global_allocs.push(Box::new(val));
+          self
+            .vars
+            .insert(var, Val::new_val_pointer(self.global_allocs.last()));
         }
         _ => panic!("invalid global variable"),
       }
@@ -53,6 +58,7 @@ impl<W: Write> Visitor<W> for Interpreter {
 impl Interpreter {
   fn new(ext_funcs: ExternFuncs) -> Self {
     Self {
+      global_allocs: Vec::new(),
       vars: HashMap::new(),
       envs: Vec::new(),
       ext_funcs,
@@ -92,23 +98,17 @@ impl Interpreter {
   }
 
   fn eval_func(&mut self, func: &Function, args: Vec<Val>) -> Result<Val> {
+    // check parameter count
+    assert_eq!(func.params().len(), args.len(), "parameter count mismatch");
     // check if is a function declaration
     if let Some(bb) = func.inner().bbs().front().get() {
       // setup the environment
-      if func.params().len() != args.len() {
-        return Err(Self::new_error(&format!(
-          "{} parameters required but got {} in function '{}'",
-          func.params().len(),
-          args.len(),
-          func.name(),
-        )));
-      }
       self.envs.push(Environment::new(
         func
           .params()
           .iter()
           .map(|p| Rc::as_ptr(p))
-          .zip(args.into_iter().map(Box::new))
+          .zip(args.into_iter())
           .collect(),
       ));
       // evaluate the entry basic block
@@ -122,19 +122,112 @@ impl Interpreter {
   }
 
   fn eval_bb(&mut self, bb: &BasicBlock) -> Result<Val> {
+    // evaluate on all instructions
+    for inst in bb.inner().insts() {
+      match inst.kind() {
+        ValueKind::Alloc(_) => self.eval_alloc(inst)?,
+        ValueKind::Load(v) => self.eval_load(inst, v)?,
+        ValueKind::Store(v) => self.eval_store(inst, v)?,
+        ValueKind::GetPtr(v) => self.eval_getptr(inst, v)?,
+        ValueKind::GetElemPtr(v) => self.eval_getelemptr(inst, v)?,
+        ValueKind::Binary(v) => self.eval_binary(inst, v)?,
+        ValueKind::Call(v) => self.eval_call(inst, v)?,
+        ValueKind::Phi(v) => self.eval_phi(inst, v)?,
+        ValueKind::Branch(v) => return self.eval_branch(bb, v),
+        ValueKind::Jump(v) => return self.eval_jump(bb, v),
+        ValueKind::Return(v) => return self.eval_return(v),
+        _ => panic!("invalid instruction"),
+      }
+    }
+    unreachable!()
+  }
+
+  fn eval_alloc(&mut self, inst: &Value) -> Result<()> {
+    let base = match inst.ty().kind() {
+      TypeKind::Pointer(base) => base,
+      _ => panic!("invalid pointer type"),
+    };
+    let env = self.envs.first_mut().unwrap();
+    env.allocs.push(Box::new(Self::new_zeroinit(base)?));
+    env
+      .vals
+      .insert(inst, Val::new_val_pointer(env.allocs.last()));
+    Ok(())
+  }
+
+  fn eval_load(&mut self, inst: &Value, load: &Load) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_store(&mut self, inst: &Value, store: &Store) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_getptr(&mut self, inst: &Value, gp: &GetPtr) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_getelemptr(&mut self, inst: &Value, gep: &GetElemPtr) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_binary(&mut self, inst: &Value, bin: &Binary) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_call(&mut self, inst: &Value, call: &Call) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_phi(&mut self, inst: &Value, phi: &Phi) -> Result<()> {
+    todo!()
+  }
+
+  fn eval_branch(&mut self, cur_bb: &BasicBlock, br: &Branch) -> Result<Val> {
+    // update last basic block
+    self.envs.first_mut().unwrap().last_bb = cur_bb;
+    // evaluate on condition
+    let cond = self.eval_value(value!(br.cond()))?;
+    // perform branching
+    if cond.as_bool() {
+      self.eval_bb(br.true_bb().upgrade().unwrap().as_ref())
+    } else {
+      self.eval_bb(br.false_bb().upgrade().unwrap().as_ref())
+    }
+  }
+
+  fn eval_jump(&mut self, cur_bb: &BasicBlock, jump: &Jump) -> Result<Val> {
+    // update last basic block
+    self.envs.first_mut().unwrap().last_bb = cur_bb;
+    // just jump
+    self.eval_bb(jump.target().upgrade().unwrap().as_ref())
+  }
+
+  fn eval_return(&mut self, ret: &Return) -> Result<Val> {
+    ret
+      .value()
+      .value()
+      .map_or(Ok(Val::Undef), |v| self.eval_value(v.as_ref()))
+  }
+
+  fn eval_value(&mut self, val: &Value) -> Result<Val> {
+    // TODO: &mut self -> &self
+    // TODO: do not use `Result` as return type
     todo!()
   }
 }
 
 struct Environment {
-  allocs: HashMap<*const Value, Box<Val>>,
+  allocs: Vec<Box<Val>>,
+  vals: HashMap<*const Value, Val>,
   last_bb: *const BasicBlock,
 }
 
 impl Environment {
-  fn new(allocs: HashMap<*const Value, Box<Val>>) -> Self {
+  fn new(vals: HashMap<*const Value, Val>) -> Self {
     Self {
-      allocs,
+      allocs: Vec::new(),
+      vals,
       last_bb: null(),
     }
   }
@@ -178,5 +271,9 @@ impl Val {
         len: 0,
       }
     }
+  }
+
+  fn as_bool(&self) -> bool {
+    matches!(self, Val::Int(i) if *i != 0)
   }
 }
