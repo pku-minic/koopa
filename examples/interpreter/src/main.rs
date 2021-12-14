f97f1ef5ad9ea24296966996889ee4f53a25ab10
MaxXSoft <x@MaxXSoft.net>
2021-12-14 13:19:03 +0800

    (main) added more implementations

--- examples/interpreter/src/main.rs
+++ examples/interpreter/src/main.rs
@@ -95,6 +95,30 @@ impl Interpreter {
     }
   }
 
+  fn get_pointer(src: Val, offset: isize, base_size: usize) -> Result<Val> {
+    match src {
+      Val::Pointer { ptr, index, len } => {
+        let new_index = (index as isize + offset) as usize;
+        (new_index < len)
+          .then(|| Val::Pointer {
+            ptr: ptr.map(|p| unsafe { NonNull::new_unchecked(p.as_ptr().offset(offset)) }),
+            index: new_index,
+            len,
+          })
+          .ok_or_else(|| {
+            new_error(&format!(
+              "pointer calculation out of bounds with index {} and length {}",
+              index, len
+            ))
+          })
+      }
+      Val::UnsafePointer(ptr) => Ok(Val::UnsafePointer(ptr.map(|p| unsafe {
+        NonNull::new_unchecked((p.as_ptr() as isize + base_size as isize * offset) as *mut ())
+      }))),
+      _ => panic!("invalid pointer"),
+    }
+  }
+
   fn eval_func(&mut self, func: &Function, args: Vec<Val>) -> Result<Val> {
     // check parameter count
     assert_eq!(func.params().len(), args.len(), "parameter count mismatch");
@@ -155,30 +179,69 @@ impl Interpreter {
   fn eval_load(&mut self, inst: &Value, load: &Load) -> Result<()> {
     let val = match self.eval_value(value!(load.src())) {
       Val::Pointer { ptr, .. } => ptr.map(|p| unsafe { p.as_ref().clone() }),
-      Val::UnsafePointer(ptr) => todo!(),
+      Val::UnsafePointer(ptr) => Val::load_from_unsafe_ptr(ptr, inst.ty()),
       _ => panic!("invalid pointer"),
     }
-    .ok_or_else(|| new_error("accessing null pointer"))?;
-    self.envs.last_mut().unwrap().vals.insert(inst, val);
+    .ok_or_else(|| new_error("accessing to null pointer"))?;
+    self.insert_val(inst, val);
     Ok(())
   }
 
   fn eval_store(&self, store: &Store) -> Result<()> {
     let val = self.eval_value(value!(store.value()));
     match self.eval_value(value!(store.dest())) {
-      Val::Pointer { ptr, .. } => ptr.map(|p| unsafe { *p.as_ptr() = val }),
-      Val::UnsafePointer(ptr) => todo!(),
+      Val::Pointer { ptr, .. } => ptr
+        .map(|p| unsafe { *p.as_ptr() = val })
+        .ok_or_else(|| new_error("accessing to null pointer")),
+      Val::UnsafePointer(ptr) => val.store_to_unsafe_ptr(ptr, value!(store.value()).ty()),
       _ => panic!("invalid pointer"),
     }
-    .ok_or_else(|| new_error("accessing null pointer"))
   }
 
   fn eval_getptr(&mut self, inst: &Value, gp: &GetPtr) -> Result<()> {
-    todo!()
+    // evaluate on index (offset)
+    let offset = match self.eval_value(value!(gp.index())) {
+      Val::Int(i) => i as isize,
+      _ => panic!("invalid index"),
+    };
+    // perform pointer calculation
+    let base_size = match inst.ty().kind() {
+      TypeKind::Pointer(base) => base.size(),
+      _ => panic!("invalid pointer"),
+    };
+    let ptr = Self::get_pointer(self.eval_value(value!(gp.src())), offset, base_size)?;
+    self.insert_val(inst, ptr);
+    Ok(())
   }
 
   fn eval_getelemptr(&mut self, inst: &Value, gep: &GetElemPtr) -> Result<()> {
-    todo!()
+    // evaluate on index (offset)
+    let offset = match self.eval_value(value!(gep.index())) {
+      Val::Int(i) => i as isize,
+      _ => panic!("invalid index"),
+    };
+    // perform pointer calculation
+    let base_size = match inst.ty().kind() {
+      TypeKind::Pointer(base) => match base.kind() {
+        TypeKind::Array(ty, _) => ty.size(),
+        _ => panic!("invalid array pointer"),
+      },
+      _ => panic!("invalid pointer"),
+    };
+    let ptr = match self.eval_value(value!(gep.src())) {
+      Val::Pointer { ptr, .. } => ptr
+        .map(|p| match unsafe { p.as_ref() } {
+          Val::Array(arr) => Self::get_pointer(Val::new_array_pointer(arr), offset, base_size),
+          _ => panic!("invalid array"),
+        })
+        .ok_or_else(|| new_error("accessing to null pointer"))??,
+      Val::UnsafePointer(ptr) => Val::UnsafePointer(ptr.map(|p| unsafe {
+        NonNull::new_unchecked((p.as_ptr() as isize + base_size as isize * offset) as *mut ())
+      })),
+      _ => panic!("invalid pointer"),
+    };
+    self.insert_val(inst, ptr);
+    Ok(())
   }
 
   fn eval_binary(&mut self, inst: &Value, bin: &Binary) {
@@ -209,12 +272,7 @@ impl Interpreter {
       BinaryOp::Shr => ((lv as u32) >> rv) as i32,
       BinaryOp::Sar => lv >> rv,
     };
-    self
-      .envs
-      .last_mut()
-      .unwrap()
-      .vals
-      .insert(inst, Val::Int(ans));
+    self.insert_val(inst, Val::Int(ans));
   }
 
   fn eval_call(&mut self, inst: &Value, call: &Call) -> Result<()> {
@@ -226,7 +284,7 @@ impl Interpreter {
       .collect();
     // perform function call
     let ret = self.eval_func(call.callee().upgrade().unwrap().as_ref(), args)?;
-    self.envs.last_mut().unwrap().vals.insert(inst, ret);
+    self.insert_val(inst, ret);
     Ok(())
   }
 
@@ -238,7 +296,7 @@ impl Interpreter {
         (self.envs.last().unwrap().last_bb == b.as_ptr()).then(|| self.eval_value(value!(o)))
       })
       .unwrap();
-    self.envs.last_mut().unwrap().vals.insert(inst, val);
+    self.insert_val(inst, val);
   }
 
   fn eval_branch(&mut self, cur_bb: &BasicBlock, br: &Branch) -> Result<Val> {
@@ -280,6 +338,10 @@ impl Interpreter {
         .unwrap()
         .clone()
     }
+  }
+
+  fn insert_val(&mut self, inst: &Value, val: Val) {
+    self.envs.last_mut().unwrap().vals.insert(inst, val);
   }
 }
 
@@ -324,23 +386,69 @@ impl Val {
     }
   }
 
-  fn new_array_pointer(parent: Option<&Box<[Val]>>) -> Self {
-    if let Some(arr) = parent {
-      Self::Pointer {
-        ptr: Some(unsafe { NonNull::new_unchecked(arr.as_ptr() as *mut Val) }),
-        index: 0,
-        len: arr.len(),
-      }
-    } else {
-      Self::Pointer {
-        ptr: None,
-        index: 0,
-        len: 0,
-      }
+  fn new_array_pointer(arr: &Box<[Val]>) -> Self {
+    Self::Pointer {
+      ptr: Some(unsafe { NonNull::new_unchecked(arr.as_ptr() as *mut Val) }),
+      index: 0,
+      len: arr.len(),
     }
+  }
+
+  fn load_from_unsafe_ptr(ptr: Option<NonNull<()>>, ty: &Type) -> Option<Self> {
+    ptr.map(|p| match ty.kind() {
+      TypeKind::Int32 => Val::Int(unsafe { *(p.as_ptr() as *const i32) }),
+      TypeKind::Array(ty, len) => Val::Array(
+        (0..*len)
+          .map(|i| {
+            Val::load_from_unsafe_ptr(
+              Some(unsafe {
+                NonNull::new_unchecked((p.as_ptr() as usize + ty.size() * i) as *mut ())
+              }),
+              ty,
+            )
+            .unwrap()
+          })
+          .collect::<Vec<_>>()
+          .into_boxed_slice(),
+      ),
+      TypeKind::Pointer(_) => Val::UnsafePointer(NonNull::new(unsafe {
+        *(p.as_ptr() as *const usize) as *mut ()
+      })),
+      _ => panic!("invalid type"),
+    })
   }
 
   fn as_bool(&self) -> bool {
     matches!(self, Val::Int(i) if *i != 0)
+  }
+
+  fn store_to_unsafe_ptr(&self, ptr: Option<NonNull<()>>, ty: &Type) -> Result<()> {
+    ptr
+      .map(|p| match self {
+        Val::Int(i) => Ok(unsafe { *(p.as_ptr() as *mut i32) = *i }),
+        Val::Array(arr) => {
+          let base = match ty.kind() {
+            TypeKind::Array(base, _) => base,
+            _ => panic!("invalid array type"),
+          };
+          arr
+            .iter()
+            .enumerate()
+            .map(|(i, v)| {
+              v.store_to_unsafe_ptr(
+                Some(unsafe {
+                  NonNull::new_unchecked((p.as_ptr() as usize + base.size() * i) as *mut ())
+                }),
+                base,
+              )
+            })
+            .collect()
+        }
+        Val::UnsafePointer(ptr) => {
+          Ok(unsafe { *(p.as_ptr() as *mut *const ()) = ptr.map_or(null(), |p| p.as_ptr()) })
+        }
+        _ => Err(new_error("incompatible type of value")),
+      })
+      .ok_or_else(|| new_error("accessing to null pointer"))?
   }
 }
