c66b7d72ff0b3420087ab09138952d4c02c658a7
MaxXSoft <x@MaxXSoft.net>
2021-12-25 10:49:48 +0800

    (idman) added module `idman` for managing IDs

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -4,6 +4,8 @@ pub mod structs;
 pub mod types;
 pub mod values;
 
+mod idman;
+
 pub use self::core::{Value, ValueKind, ValueRc};
 pub use structs::{BasicBlock, BasicBlockRc, Function, FunctionRc, Program};
 pub use types::{Type, TypeKind};
