02f3cbe96edc02789ff848b384e424389f22cdb3
MaxXSoft <x@MaxXSoft.net>
2021-12-28 12:46:16 +0800

    (mod) updated module `ir`

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -7,6 +7,5 @@ pub mod values;
 
 mod idman;
 
-// pub use self::core::{Value, ValueKind, ValueRc};
-// pub use structs::{BasicBlock, BasicBlockRc, Function, FunctionRc, Program};
-// pub use types::{Type, TypeKind};
+pub use entities::{BasicBlock, Function, FunctionData, Program, Value, ValueKind};
+pub use types::{Type, TypeKind};
