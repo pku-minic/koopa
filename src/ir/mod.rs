c6763ef1b6285f70ed6c085efdbbeb185c36bdd2
MaxXSoft <x@MaxXSoft.net>
2021-12-28 12:39:39 +0800

    (mod) updated module `ir`

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,11 +1,12 @@
-pub mod core;
-pub mod instructions;
-pub mod structs;
+pub mod builder;
+pub mod dfg;
+pub mod entities;
+pub mod layout;
 pub mod types;
 pub mod values;
 
 mod idman;
 
-pub use self::core::{Value, ValueKind, ValueRc};
-pub use structs::{BasicBlock, BasicBlockRc, Function, FunctionRc, Program};
-pub use types::{Type, TypeKind};
+// pub use self::core::{Value, ValueKind, ValueRc};
+// pub use structs::{BasicBlock, BasicBlockRc, Function, FunctionRc, Program};
+// pub use types::{Type, TypeKind};
