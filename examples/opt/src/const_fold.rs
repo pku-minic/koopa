01964326cc20baac298a86da838918fb71529a41
MaxXSoft <x@MaxXSoft.net>
2022-01-01 10:46:39 +0800

    (const_fold) updated some implementations

--- examples/opt/src/const_fold.rs
+++ examples/opt/src/const_fold.rs
@@ -1,32 +1,15 @@
-use koopa::ir::instructions::{Binary, BinaryOp, Phi};
-use koopa::ir::values::{Integer, Undef};
-use koopa::ir::{Function, Type, ValueKind, ValueRc};
+use koopa::ir::{builder_traits::*, BinaryOp, Function, FunctionData, Type, ValueKind};
 use koopa::opt::FunctionPass;
-use std::rc::Rc;
 
 /// Performs constant folding.
 pub struct ConstantFolding;
 
 impl FunctionPass for ConstantFolding {
-  fn run_on(&mut self, func: &Function) {
-    let mut changed = true;
-    while changed {
-      changed = false;
-      // scan all basic blocks
-      for bb in func.inner().bbs() {
-        for inst in bb.inner().insts() {
-          let ans = match inst.kind() {
-            ValueKind::Binary(binary) => self.handle_binary(binary),
-            ValueKind::Phi(phi) => self.handle_phi(phi),
-            _ => continue,
-          };
-          // update the current instruction
-          if ans.is_some() && inst.inner_mut().replace_all_uses_with(ans) {
-            changed = true;
-          }
-        }
-      }
-    }
+  fn run_on(&mut self, _: Function, data: &mut FunctionData) {
+    // handle all binary instructions
+    while self.eval_const(data) {}
+    // handle all basic block parameters
+    self.eval_bb_params(data);
   }
 }
 
@@ -35,57 +18,102 @@ impl ConstantFolding {
     Self
   }
 
-  fn handle_binary(&mut self, binary: &Binary) -> Option<ValueRc> {
-    let lhs = binary.lhs().value().unwrap();
-    let rhs = binary.rhs().value().unwrap();
-    Some(match (lhs.kind(), rhs.kind()) {
-      (ValueKind::Integer(l), ValueKind::Integer(r)) => match binary.op() {
-        BinaryOp::NotEq => Integer::get((l.value() != r.value()) as i32),
-        BinaryOp::Eq => Integer::get((l.value() == r.value()) as i32),
-        BinaryOp::Gt => Integer::get((l.value() > r.value()) as i32),
-        BinaryOp::Lt => Integer::get((l.value() < r.value()) as i32),
-        BinaryOp::Ge => Integer::get((l.value() >= r.value()) as i32),
-        BinaryOp::Le => Integer::get((l.value() <= r.value()) as i32),
-        BinaryOp::Add => Integer::get(l.value() + r.value()),
-        BinaryOp::Sub => Integer::get(l.value() - r.value()),
-        BinaryOp::Mul => Integer::get(l.value() * r.value()),
-        BinaryOp::Div => {
-          if r.value() == 0 {
-            Undef::get(Type::get_i32())
-          } else {
-            Integer::get(l.value() / r.value())
+  fn eval_const(&self, data: &mut FunctionData) -> bool {
+    // find all evaluable binary instructions and evaluate
+    let mut evaluated = Vec::new();
+    for (v, value) in data.dfg().values() {
+      let ans = match value.kind() {
+        ValueKind::Binary(bin) => {
+          let lhs = data.dfg().value(bin.lhs()).kind();
+          let rhs = data.dfg().value(bin.rhs()).kind();
+          match (lhs, rhs) {
+            (ValueKind::Integer(l), ValueKind::Integer(r)) => match bin.op() {
+              BinaryOp::NotEq => Some((l.value() != r.value()) as i32),
+              BinaryOp::Eq => Some((l.value() == r.value()) as i32),
+              BinaryOp::Gt => Some((l.value() > r.value()) as i32),
+              BinaryOp::Lt => Some((l.value() < r.value()) as i32),
+              BinaryOp::Ge => Some((l.value() >= r.value()) as i32),
+              BinaryOp::Le => Some((l.value() <= r.value()) as i32),
+              BinaryOp::Add => Some(l.value() + r.value()),
+              BinaryOp::Sub => Some(l.value() - r.value()),
+              BinaryOp::Mul => Some(l.value() * r.value()),
+              BinaryOp::Div => (r.value() != 0).then(|| l.value() / r.value()),
+              BinaryOp::Mod => (r.value() != 0).then(|| l.value() % r.value()),
+              BinaryOp::And => Some(l.value() & r.value()),
+              BinaryOp::Or => Some(l.value() | r.value()),
+              BinaryOp::Xor => Some(l.value() ^ r.value()),
+              BinaryOp::Shl => Some(l.value() << r.value()),
+              BinaryOp::Shr => Some((l.value() as u32 >> r.value()) as i32),
+              BinaryOp::Sar => Some(l.value() >> r.value()),
+            },
+            (ValueKind::Undef(_), _) => todo!(),
+            (_, ValueKind::Undef(_)) => todo!(),
+            _ => continue,
           }
         }
-        BinaryOp::Mod => {
-          if r.value() == 0 {
-            Undef::get(Type::get_i32())
-          } else {
-            Integer::get(l.value() % r.value())
-          }
-        }
-        BinaryOp::And => Integer::get(l.value() & r.value()),
-        BinaryOp::Or => Integer::get(l.value() | r.value()),
-        BinaryOp::Xor => Integer::get(l.value() ^ r.value()),
-        BinaryOp::Shl => Integer::get(l.value() << r.value()),
-        BinaryOp::Shr => Integer::get((l.value() as u32 >> r.value()) as i32),
-        BinaryOp::Sar => Integer::get(l.value() >> r.value()),
-      },
-      (ValueKind::Undef(_), _) | (_, ValueKind::Undef(_)) => Undef::get(Type::get_i32()),
-      _ => return None,
-    })
+        _ => continue,
+      };
+      evaluated.push((*v, ans, data.layout().parent_bb(*v).unwrap()));
+    }
+    // updated values
+    let changed = !evaluated.is_empty();
+    // replace the evaluated instructions
+    for (v, ans, _) in &evaluated {
+      let builder = data.dfg_mut().replace_value_with(*v);
+      if let Some(v) = ans {
+        builder.integer(*v);
+      } else {
+        builder.undef(Type::get_i32());
+      }
+    }
+    // remove constant values in instruction layout
+    for (v, _, bb) in evaluated {
+      data.layout_mut().bb_mut(bb).insts_mut().remove(&v);
+    }
+    changed
   }
 
-  fn handle_phi(&mut self, phi: &Phi) -> Option<ValueRc> {
-    // check if all phi operands are the same value
-    phi
-      .oprs()
-      .windows(2)
-      .all(|it| {
-        let pa = it[0].0.value().unwrap();
-        let pb = it[1].0.value().unwrap();
-        Rc::ptr_eq(&pa, &pb)
-      })
-      .then(|| phi.oprs().first().unwrap().0.value())
-      .flatten()
+  fn eval_bb_params(&self, data: &mut FunctionData) {
+    let mut bb_params = Vec::new();
+    for (b, bb) in data.dfg().bbs() {
+      // collect parameters that can be evaluated in the current basic block
+      let mut evaluated = Vec::new();
+      'outer: for (i, param) in bb.params().iter().enumerate() {
+        let mut ans = None;
+        // check if all corresponding arguments are constant
+        for user in data.dfg().value(*param).used_by() {
+          // get the argument value handle
+          let value = match data.dfg().value(*user).kind() {
+            ValueKind::Branch(branch) => {
+              if branch.true_bb() == *b {
+                branch.true_args()[i]
+              } else {
+                branch.false_args()[i]
+              }
+            }
+            ValueKind::Jump(jump) => jump.args()[i],
+            _ => panic!("invalid branch/jump instruction"),
+          };
+          // check if is constant
+          let value = data.dfg().value(value);
+          if !value.kind().is_const() || !ans.map_or(true, |v| data.dfg().data_eq(&v, &value)) {
+            continue 'outer;
+          }
+          ans = Some(value.clone());
+        }
+        evaluated.push((i, ans.unwrap()));
+      }
+      if !evaluated.is_empty() {
+        bb_params.push((*b, evaluated));
+      }
+    }
+    // update basic block parameters
+    for (bb, evals) in bb_params {
+      // replace all parameters to constants
+      for (i, value) in evals {
+        let param = data.dfg().bb(bb).params()[i];
+        data.dfg_mut().replace_value_with(param).raw(value);
+      }
+    }
   }
 }
