07530bf2112ed4ddc5e8f6cf05cbc14dbcf624d5
MaxXSoft <x@MaxXSoft.net>
2022-01-01 17:09:19 +0800

    (dce) updated some implementations

--- examples/opt/src/dce.rs
+++ examples/opt/src/dce.rs
@@ -1,18 +1,20 @@
-use koopa::ir::{Function, Value, ValueKind, ValueRc};
+use koopa::ir::{builder_traits::*, Function, FunctionData, Value, ValueKind};
 use koopa::opt::FunctionPass;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 /// Performs dead code elimination.
 pub struct DeadCodeElimination {
-  worklist: Vec<ValueRc>,
-  liveset: HashSet<*const Value>,
+  worklist: Vec<Value>,
+  liveset: HashSet<Value>,
 }
 
 impl FunctionPass for DeadCodeElimination {
-  fn run_on(&mut self, func: &Function) {
-    if !func.inner().bbs().is_empty() {
-      self.mark(func);
-      self.sweep(func);
+  fn run_on(&mut self, _: Function, data: &mut FunctionData) {
+    let mut changed = true;
+    while changed {
+      self.mark(data);
+      self.sweep(data);
+      changed = self.opt_bb_params(data);
     }
   }
 }
@@ -25,53 +27,116 @@ impl DeadCodeElimination {
     }
   }
 
-  fn mark(&mut self, func: &Function) {
-    // iterate through all blocks to find critical instructions
-    for bb in func.inner().bbs() {
-      let bb = bb.inner();
-      let mut cur = bb.insts().front();
-      while let Some(inst) = cur.clone_pointer() {
-        if Self::is_critical_inst(&inst) {
-          // mark as undead
-          self.liveset.insert(inst.as_ref());
-          self.worklist.push(inst);
-        }
-        cur.move_next();
+  fn mark(&mut self, data: &FunctionData) {
+    // iterate through all values to find critical instructions
+    for (v, value) in data.dfg().values() {
+      if Self::is_critical_inst(value.kind()) {
+        self.liveset.insert(*v);
+        self.worklist.push(*v);
       }
     }
     // mark more instructions
     while let Some(inst) = self.worklist.pop() {
       // mark all of its operand as undead
-      for u in inst.uses() {
-        if let Some(value) = u.value() {
-          let ptr: *const Value = value.as_ref();
-          if !self.liveset.contains(&ptr) && value.is_inst() {
-            self.liveset.insert(ptr);
-            self.worklist.push(value);
-          }
+      for u in data.dfg().value(inst).kind().value_uses() {
+        if !self.liveset.contains(&u) && data.dfg().value(u).kind().is_local_inst() {
+          self.liveset.insert(u);
+          self.worklist.push(u);
         }
       }
     }
   }
 
-  fn sweep(&mut self, func: &Function) {
-    // iterate through all blocks
-    for bb in func.inner().bbs() {
-      let mut bb_inner = bb.inner_mut();
-      let mut cur = bb_inner.front_mut();
-      while let Some(inst) = cur.get() {
-        if !self.liveset.contains(&(inst as *const Value)) {
-          cur.remove();
+  fn sweep(&mut self, data: &mut FunctionData) {
+    // remove instructions in layout
+    let mut removed = Vec::new();
+    let mut bb_cur = data.layout_mut().bbs_mut().cursor_front_mut();
+    while let Some(bb) = bb_cur.node_mut() {
+      let mut inst_cur = bb.insts_mut().cursor_front_mut();
+      while let Some(inst) = inst_cur.key() {
+        if !self.liveset.contains(inst) {
+          removed.push(*inst);
+          inst_cur.remove_current();
         } else {
-          cur.move_next();
+          inst_cur.move_next();
         }
       }
+      bb_cur.move_next();
+    }
+    // remove values in DFG
+    for v in removed {
+      data.dfg_mut().remove_value(v);
     }
   }
 
-  fn is_critical_inst(inst: &ValueRc) -> bool {
+  fn opt_bb_params(&self, data: &mut FunctionData) -> bool {
+    // get map: bb that should be handled ->
+    //          (index of param that should be removed -> param value)
+    let bbs: HashMap<_, _> = data
+      .dfg()
+      .bbs()
+      .iter()
+      .filter_map(|(b, bb)| {
+        let unused: HashMap<_, _> = bb
+          .params()
+          .iter()
+          .enumerate()
+          .filter_map(|(i, p)| data.dfg().value(*p).used_by().is_empty().then(|| (i, *p)))
+          .collect();
+        (!unused.is_empty()).then(|| (*b, unused))
+      })
+      .collect();
+    let changed = !bbs.is_empty();
+    // remove unused parameters of all detected basic blocks
+    bbs.iter().for_each(|(b, m)| {
+      let mut index = 0;
+      data.dfg_mut().bb_mut(*b).params_mut().retain(|_| {
+        index += 1;
+        !m.contains_key(&(index - 1))
+      });
+      m.iter().for_each(|(_, param)| {
+        data.dfg_mut().remove_value(*param);
+      });
+    });
+    // update arguments of all related branch/jump instructions
+    bbs.into_iter().for_each(|(b, m)| {
+      let users = data.dfg().bb(b).used_by().clone();
+      users.into_iter().for_each(|user| {
+        // remove instruction from DFG
+        let mut inst = data.dfg_mut().remove_value(user);
+        // get argument list
+        let args = match inst.kind_mut() {
+          ValueKind::Branch(br) => {
+            if br.true_bb() == b {
+              br.true_args_mut()
+            } else {
+              br.false_args_mut()
+            }
+          }
+          ValueKind::Jump(jump) => jump.args_mut(),
+          _ => panic!("invalid branch/jump instruction"),
+        };
+        // update argument list
+        let mut index = 0;
+        args.retain(|a| {
+          index += 1;
+          let removed = !m.contains_key(&(index - 1));
+          if removed && data.dfg().value(*a).used_by().is_empty() {
+            // remove unused values
+            data.dfg_mut().remove_value(*a);
+          }
+          removed
+        });
+        // add instruction to DFG
+        data.dfg_mut().new_value().raw(inst);
+      });
+    });
+    changed
+  }
+
+  fn is_critical_inst(kind: &ValueKind) -> bool {
     matches!(
-      inst.kind(),
+      kind,
       ValueKind::Store(_)
         | ValueKind::Call(_)
         | ValueKind::Branch(_)
