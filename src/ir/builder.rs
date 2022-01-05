fd1e2362b0f737bc9701876c52401d380b880ae4
MaxXSoft <x@MaxXSoft.net>
2022-01-05 09:29:18 +0800

    (ir/builder) added some docs

--- src/ir/builder.rs
+++ src/ir/builder.rs
@@ -1,3 +1,10 @@
+//! Koopa IR builders and IR builder traits.
+//!
+//! Methods like [`Program::new_value`], [`DataFlowGraph::new_value`] or
+//! [`DataFlowGraph::replace_value_with`] will return an IR builder object.
+//! You can only create values or basic blocks by using the interface
+//! provided by the builder traits.
+
 use crate::ir::dfg::DataFlowGraph;
 use crate::ir::entities::{BasicBlock, BasicBlockData, Function, Program, Value, ValueData};
 use crate::ir::types::{Type, TypeKind};
@@ -472,6 +479,8 @@ impl<T: DfgBasedInfoQuerier> EntityInfoQuerier for T {
 
 /// An value builder that builds a new local value and inserts it
 /// to the data flow graph.
+/// 
+/// Returned by method [`DataFlowGraph::new_value`].
 pub struct LocalBuilder<'a> {
   pub(in crate::ir) dfg: &'a mut DataFlowGraph,
 }
@@ -493,6 +502,8 @@ impl<'a> LocalInstBuilder for LocalBuilder<'a> {}
 
 /// An basic block builder that builds a new basic block and inserts it
 /// to the data flow graph.
+/// 
+/// Returned by method [`DataFlowGraph::new_bb`].
 pub struct BlockBuilder<'a> {
   pub(in crate::ir) dfg: &'a mut DataFlowGraph,
 }
@@ -510,8 +521,9 @@ impl<'a> BasicBlockBuilder for BlockBuilder<'a> {
 }
 
 /// An value builder that replaces an existing local value.
-///
 /// The inserted new value will have the same value handle as the old one.
+/// 
+/// Returned by method [`DataFlowGraph::replace_value_with`].
 pub struct ReplaceBuilder<'a> {
   pub(in crate::ir) dfg: &'a mut DataFlowGraph,
   pub(in crate::ir) value: Value,
@@ -535,6 +547,8 @@ impl<'a> LocalInstBuilder for ReplaceBuilder<'a> {}
 
 /// An value builder that builds a new global value and inserts it
 /// to the program.
+/// 
+/// Returned by method [`Program::new_value`].
 pub struct GlobalBuilder<'a> {
   pub(in crate::ir) program: &'a mut Program,
 }
