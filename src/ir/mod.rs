a525d90e8c9816244849704b7e48a18951561725
MaxXSoft <x@MaxXSoft.net>
2022-01-05 09:26:37 +0800

    (ir/mod) added some docs

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,3 +1,14 @@
+//! The core part of Koopa IR.
+//!
+//! This module provides in-memory form IR related implementations,
+//! containing:
+//!
+//! * Structs of Koopa IR programs ([`Program`]), functions ([`Function`],
+//!   [`FunctionData`]), basic blocks ([`BasicBlock`], [`BasicBlockData`])
+//!   and values ([`Value`], [`ValueData`]).
+//! * Types of IR values ([`Type`]).
+//! * IR builders and IR builder traits ([`builder`]).
+
 pub mod builder;
 pub mod dfg;
 pub mod entities;
@@ -8,6 +19,19 @@ pub mod values;
 mod idman;
 
 pub mod builder_traits {
+  //! Re-exportations of IR builder traits.
+  //!
+  //! # Usage
+  //!
+  //! ```
+  //! use koopa::ir::{FunctionData, Type};
+  //! use koopa::ir::builder_traits::*;
+  //!
+  //! let mut func = FunctionData::new("@func", Vec::new(), Type::get_unit());
+  //! // builder traits required
+  //! let zero = func.dfg_mut().new_value().integer(0);
+  //! ```
+
   pub use super::builder::{BasicBlockBuilder, GlobalInstBuilder, LocalInstBuilder, ValueBuilder};
 }
 
