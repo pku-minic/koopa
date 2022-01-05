96bfbd17037e32bc2d05df64340c492a38e2a7fc
MaxXSoft <x@MaxXSoft.net>
2022-01-05 13:07:08 +0800

    (ir/mod) added some examples

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -8,6 +8,72 @@
 //!   and values ([`Value`], [`ValueData`]).
 //! * Types of IR values ([`Type`]).
 //! * IR builders and IR builder traits ([`builder`]).
+//!
+//! # Example
+//!
+//! Here is a Fibonacci function represented in Koopa IR:
+//!
+//! ```koopa
+//! fun @fib(@n: i32): i32 {
+//! %entry:
+//!   %cond = le @n, 2
+//!   br %cond, %then, %else
+//!
+//! %then:
+//!   ret 1
+//!
+//! %else:
+//!   %0 = sub @n, 1
+//!   %x = call @fib(%0)
+//!   %1 = sub @n, 2
+//!   %y = call @fib(%1)
+//!   %ans = add %x, %y
+//!   ret %ans
+//! }
+//! ```
+//!
+//! You can build the corresponding Koopa IR program:
+//!
+//! ```
+//! use koopa::ir::*;
+//! use koopa::ir::builder_traits::*;
+//!
+//! // create program and function
+//! let mut program = Program::new();
+//! let fib = program.new_func(FunctionData::with_param_names(
+//!   "@fib".into(),
+//!   vec![(Some("@n".into()), Type::get_i32())],
+//!   Type::get_i32(),
+//! ));
+//! let fib_data = program.func_mut(fib);
+//! let n = fib_data.params()[0];
+//!
+//! // entry/then/else basic block
+//! let entry = fib_data.dfg_mut().new_bb().basic_block(Some("%entry".into()));
+//! let then = fib_data.dfg_mut().new_bb().basic_block(Some("%then".into()));
+//! let else_bb = fib_data.dfg_mut().new_bb().basic_block(Some("%else".into()));
+//! fib_data.layout_mut().bbs_mut().extend([entry, then, else_bb]);
+//!
+//! // instructions in entry basic block
+//! let two = fib_data.dfg_mut().new_value().integer(2);
+//! let cond = fib_data.dfg_mut().new_value().binary(BinaryOp::Le, n, two);
+//! let br = fib_data.dfg_mut().new_value().branch(cond, then, else_bb);
+//! fib_data.layout_mut().bb_mut(entry).insts_mut().extend([cond, br]);
+//!
+//! // instructions in `then` basic block
+//! let one = fib_data.dfg_mut().new_value().integer(1);
+//! let ret = fib_data.dfg_mut().new_value().ret(Some(one));
+//! fib_data.layout_mut().bb_mut(entry).insts_mut().push_key_back(ret);
+//!
+//! // instructions in `else` basic block
+//! let sub1 = fib_data.dfg_mut().new_value().binary(BinaryOp::Sub, n, one);
+//! let call1 = fib_data.dfg_mut().new_value().call(fib, vec![sub1]);
+//! let sub2 = fib_data.dfg_mut().new_value().binary(BinaryOp::Sub, n, two);
+//! let call2 = fib_data.dfg_mut().new_value().call(fib, vec![sub2]);
+//! let ans = fib_data.dfg_mut().new_value().binary(BinaryOp::Add, call1, call2);
+//! let ret = fib_data.dfg_mut().new_value().ret(Some(ans));
+//! fib_data.layout_mut().bb_mut(entry).insts_mut().extend([sub1, call1, sub2, call2, ans, ret]);
+//! ```
 
 pub mod builder;
 pub mod dfg;
@@ -27,7 +93,7 @@ pub mod builder_traits {
   //! use koopa::ir::{FunctionData, Type};
   //! use koopa::ir::builder_traits::*;
   //!
-  //! let mut func = FunctionData::new("@func", Vec::new(), Type::get_unit());
+  //! let mut func = FunctionData::new("@func".into(), Vec::new(), Type::get_unit());
   //! // builder traits required
   //! let zero = func.dfg_mut().new_value().integer(0);
   //! ```
