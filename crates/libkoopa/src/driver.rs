75ef459c97fc5817e155428f615e779bc1516880
MaxXSoft <x@MaxXSoft.net>
2022-01-08 21:45:30 +0800

    (driver) added function `koopa_parse_from_raw`

--- crates/libkoopa/src/driver.rs
+++ crates/libkoopa/src/driver.rs
@@ -1,6 +1,7 @@
 use crate::errors::{unwrap_or_return, ErrorCode};
+use crate::io::{file_from_raw, RawFile};
 use crate::utils::{drop_pointer, ffi, new_pointer};
-use koopa::front::Driver;
+use koopa::front::{span::FileType, Driver};
 use koopa::ir::Program;
 use std::ffi::CStr;
 use std::io::stdin;
@@ -37,6 +38,19 @@ ffi! {
   /// Returns the error code.
   fn koopa_parse_from_stdin(program: &mut *mut Program) -> ErrorCode {
     let driver = Driver::from(stdin());
+    let prog = unwrap_or_return!(driver.generate_program(), InvalidKoopaProgram);
+    *program = new_pointer(prog);
+    ErrorCode::Success
+  }
+
+  /// Parses text-form Koopa IR program from the given
+  /// file descriptor (UNIX) or handle (Windows).
+  /// Updates the `program` if no error occurred.
+  ///
+  /// Returns the error code.
+  fn koopa_parse_from_raw(file: RawFile, program: &mut *mut Program) -> ErrorCode {
+    let file = file_from_raw(file);
+    let driver = Driver::new(FileType::Buffer, file);
     let prog = unwrap_or_return!(driver.generate_program(), InvalidKoopaProgram);
     *program = new_pointer(prog);
     ErrorCode::Success
