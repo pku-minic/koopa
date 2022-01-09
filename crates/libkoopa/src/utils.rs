3db221ece019988f1f2df57c79082c22e83a64dd
MaxXSoft <x@MaxXSoft.net>
2022-01-09 19:31:43 +0800

    (utils) added function `new_uninit_box`

--- crates/libkoopa/src/utils.rs
+++ crates/libkoopa/src/utils.rs
@@ -1,3 +1,5 @@
+use std::mem::MaybeUninit;
+
 /// Creates a new object `t` and returns its pointer.
 pub(crate) fn new_pointer<T>(t: T) -> *mut T {
   Box::into_raw(Box::new(t))
@@ -6,6 +8,16 @@ pub(crate) fn new_pointer<T>(t: T) -> *mut T {
 /// Drops the given pointer that created by [`new_pointer`].
 pub(crate) fn drop_pointer<T>(ptr: *mut T) {
   unsafe { Box::from_raw(ptr) };
+}
+
+/// Returns a new uninitialized box.
+///
+/// # Safety
+///
+/// The returned box must be initialized before it can be read.
+pub(crate) unsafe fn new_uninit_box<T>() -> Box<T> {
+  let b = Box::new(MaybeUninit::<T>::uninit());
+  Box::from_raw(Box::into_raw(b) as *mut T)
 }
 
 /// Defines FFI functions.
