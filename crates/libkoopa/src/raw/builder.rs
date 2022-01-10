bc9234bb89b1c8e78fb57a7614aa8e68ed67572a
MaxXSoft <x@MaxXSoft.net>
2022-01-10 08:51:33 +0800

    (builder) added more implementations

--- crates/libkoopa/src/raw/builder.rs
+++ crates/libkoopa/src/raw/builder.rs
@@ -1,7 +1,9 @@
 use super::entities::*;
 use crate::utils::new_uninit_box;
 use koopa::ir::entities::{BasicBlockData, ValueData};
-use koopa::ir::{BasicBlock, Function, FunctionData, Program, Type, TypeKind, Value, ValueKind};
+use koopa::ir::values::*;
+use koopa::ir::{BasicBlock, Function, FunctionData, Program, Type, TypeKind};
+use koopa::ir::{BinaryOp, Value, ValueKind};
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::os::raw::{c_char, c_void};
@@ -266,7 +268,194 @@ impl BuildRaw for ValueKind {
   type Raw = RawValueKind;
 
   fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
-    todo!()
+    match self {
+      ValueKind::Integer(v) => RawValueKind::Integer(v.build(builder, info)),
+      ValueKind::ZeroInit(_) => RawValueKind::ZeroInit,
+      ValueKind::Undef(_) => RawValueKind::Undef,
+      ValueKind::Aggregate(v) => RawValueKind::Aggregate(v.build(builder, info)),
+      ValueKind::FuncArgRef(v) => RawValueKind::FuncArgRef(v.build(builder, info)),
+      ValueKind::BlockArgRef(v) => RawValueKind::BlockArgRef(v.build(builder, info)),
+      ValueKind::Alloc(_) => RawValueKind::Alloc,
+      ValueKind::GlobalAlloc(v) => RawValueKind::GlobalAlloc(v.build(builder, info)),
+      ValueKind::Load(v) => RawValueKind::Load(v.build(builder, info)),
+      ValueKind::Store(v) => RawValueKind::Store(v.build(builder, info)),
+      ValueKind::GetPtr(v) => RawValueKind::GetPtr(v.build(builder, info)),
+      ValueKind::GetElemPtr(v) => RawValueKind::GetElemPtr(v.build(builder, info)),
+      ValueKind::Binary(v) => RawValueKind::Binary(v.build(builder, info)),
+      ValueKind::Branch(v) => RawValueKind::Branch(v.build(builder, info)),
+      ValueKind::Jump(v) => RawValueKind::Jump(v.build(builder, info)),
+      ValueKind::Call(v) => RawValueKind::Call(v.build(builder, info)),
+      ValueKind::Return(v) => RawValueKind::Return(v.build(builder, info)),
+    }
+  }
+}
+
+impl BuildRaw for Integer {
+  type Raw = RawInteger;
+
+  fn build(&self, _: &mut RawProgramBuilder, _: &mut ProgramInfo) -> Self::Raw {
+    RawInteger {
+      value: self.value(),
+    }
+  }
+}
+
+impl BuildRaw for Aggregate {
+  type Raw = RawAggregate;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawAggregate {
+      elems: iter_into_raw(self.elems().iter(), builder, info),
+    }
+  }
+}
+
+impl BuildRaw for FuncArgRef {
+  type Raw = RawFuncArgRef;
+
+  fn build(&self, _: &mut RawProgramBuilder, _: &mut ProgramInfo) -> Self::Raw {
+    RawFuncArgRef {
+      index: self.index(),
+    }
+  }
+}
+
+impl BuildRaw for BlockArgRef {
+  type Raw = RawBlockArgRef;
+
+  fn build(&self, _: &mut RawProgramBuilder, _: &mut ProgramInfo) -> Self::Raw {
+    RawBlockArgRef {
+      index: self.index(),
+    }
+  }
+}
+
+impl BuildRaw for GlobalAlloc {
+  type Raw = RawGlobalAlloc;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawGlobalAlloc {
+      init: self.init().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Load {
+  type Raw = RawLoad;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawLoad {
+      src: self.src().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Store {
+  type Raw = RawStore;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawStore {
+      value: self.value().build(builder, info),
+      dest: self.dest().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for GetPtr {
+  type Raw = RawGetPtr;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawGetPtr {
+      src: self.src().build(builder, info),
+      index: self.index().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for GetElemPtr {
+  type Raw = RawGetElemPtr;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawGetElemPtr {
+      src: self.src().build(builder, info),
+      index: self.index().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Binary {
+  type Raw = RawBinary;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawBinary {
+      op: match self.op() {
+        BinaryOp::NotEq => RawBinaryOp::NotEq,
+        BinaryOp::Eq => RawBinaryOp::Eq,
+        BinaryOp::Gt => RawBinaryOp::Gt,
+        BinaryOp::Lt => RawBinaryOp::Lt,
+        BinaryOp::Ge => RawBinaryOp::Ge,
+        BinaryOp::Le => RawBinaryOp::Le,
+        BinaryOp::Add => RawBinaryOp::Add,
+        BinaryOp::Sub => RawBinaryOp::Sub,
+        BinaryOp::Mul => RawBinaryOp::Mul,
+        BinaryOp::Div => RawBinaryOp::Div,
+        BinaryOp::Mod => RawBinaryOp::Mod,
+        BinaryOp::And => RawBinaryOp::And,
+        BinaryOp::Or => RawBinaryOp::Or,
+        BinaryOp::Xor => RawBinaryOp::Xor,
+        BinaryOp::Shl => RawBinaryOp::Shl,
+        BinaryOp::Shr => RawBinaryOp::Shr,
+        BinaryOp::Sar => RawBinaryOp::Sar,
+      },
+      lhs: self.lhs().build(builder, info),
+      rhs: self.rhs().build(builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Branch {
+  type Raw = RawBranch;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawBranch {
+      cond: self.cond().build(builder, info),
+      true_bb: self.true_bb().build(builder, info),
+      false_bb: self.false_bb().build(builder, info),
+      true_args: iter_into_raw(self.true_args().iter(), builder, info),
+      false_args: iter_into_raw(self.false_args().iter(), builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Jump {
+  type Raw = RawJump;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawJump {
+      target: self.target().build(builder, info),
+      args: iter_into_raw(self.args().iter(), builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Call {
+  type Raw = RawCall;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawCall {
+      callee: self.callee().build(builder, info),
+      args: iter_into_raw(self.args().iter(), builder, info),
+    }
+  }
+}
+
+impl BuildRaw for Return {
+  type Raw = RawReturn;
+
+  fn build(&self, builder: &mut RawProgramBuilder, info: &mut ProgramInfo) -> Self::Raw {
+    RawReturn {
+      value: self.value().build(builder, info),
+    }
   }
 }
 
