f7d532ef5299d826976eac7355094d10ce4bf2d5
MaxXSoft <x@MaxXSoft.net>
2023-06-02 10:19:15 +0800

    (back/llvm) updated unit tests for module `llvm`

--- src/back/llvm.rs
+++ src/back/llvm.rs
@@ -541,13 +541,13 @@ mod test {
       r#"@x = global [10 x i32] zeroinitializer
 
 define i32 @test(i32 %i) {
-_entry:
-  %_0 = getelementptr inbounds [10 x i32], [10 x i32]* @x, i32 0
-  store [10 x i32] [i32 1, i32 2, i32 3, i32 4, i32 5, i32 0, i32 0, i32 0, i32 0, i32 10], [10 x i32]* %_0
-  %_1 = getelementptr inbounds [10 x i32], [10 x i32]* @x, i32 0, i32 %i
-  %_2 = load i32, i32* %_1
-  %_3 = mul i32 %_2, 7
-  ret i32 %_3
+$entry:
+  %$0 = getelementptr inbounds [10 x i32], [10 x i32]* @x, i32 0
+  store [10 x i32] [i32 1, i32 2, i32 3, i32 4, i32 5, i32 0, i32 0, i32 0, i32 0, i32 10], [10 x i32]* %$0
+  %$1 = getelementptr inbounds [10 x i32], [10 x i32]* @x, i32 0, i32 %i
+  %$2 = load i32, i32* %$1
+  %$3 = mul i32 %$2, 7
+  ret i32 %$3
 }
 "#
     );
@@ -588,25 +588,25 @@ _entry:
       r#"declare i32 @getint()
 
 define i32 @main() {
-_entry:
-  %_ans_0 = call i32 @getint()
-  br label %_while_entry
+$entry:
+  %$ans_0 = call i32 @getint()
+  br label %$while_entry
 
-_while_entry:
-  %_ind_var_0 = 
-  %_ans_1 = 
-  %_0 = icmp slt i32 %_ind_var_0, 10
-  %_cond = zext i1 %_0 to i32
-  %_1 = icmp ne i32 %_cond, 0
-  br i1 %_1, label %_while_body, label %_while_end
+$while_entry:
+  %$ind_var_0 = 
+  %$ans_1 = 
+  %$0 = icmp slt i32 %$ind_var_0, 10
+  %$cond = zext i1 %$0 to i32
+  %$1 = icmp ne i32 %$cond, 0
+  br i1 %$1, label %$while_body, label %$while_end
 
-_while_body:
-  %_ans_2 = add i32 %_ans_1, %_ind_var_0
-  %_ind_var_1 = add i32 %_ind_var_0, 1
-  br label %_while_entry
+$while_body:
+  %$ans_2 = add i32 %$ans_1, %$ind_var_0
+  %$ind_var_1 = add i32 %$ind_var_0, 1
+  br label %$while_entry
 
-_while_end:
-  ret i32 %_ans_1
+$while_end:
+  ret i32 %$ans_1
 }
 "#
     );
@@ -659,42 +659,42 @@ fun @main(): i32 {
       r#"declare i32 @getint()
 
 define i32 @main() {
-_args_0:
-  %_0 = call i32 @getint()
-  %_1 = call i32 @getint()
-  br label %_while_cond_2
+$args_0:
+  %$0 = call i32 @getint()
+  %$1 = call i32 @getint()
+  br label %$while_cond_2
 
-_while_cond_2:
-  %_2 = 
-  %_4 = 
-  %_6 = icmp slt i32 %_4, %_1
-  %_7 = zext i1 %_6 to i32
-  %_8 = icmp ne i32 %_7, 0
-  br i1 %_8, label %_while_body_3, label %_while_end_1
+$while_cond_2:
+  %$2 = 
+  %$4 = 
+  %$6 = icmp slt i32 %$4, %$1
+  %$7 = zext i1 %$6 to i32
+  %$8 = icmp ne i32 %$7, 0
+  br i1 %$8, label %$while_body_3, label %$while_end_1
 
-_while_body_3:
-  br label %_while_cond_4
+$while_body_3:
+  br label %$while_cond_4
 
-_while_end_1:
-  ret i32 %_2
+$while_end_1:
+  ret i32 %$2
 
-_while_cond_4:
-  %_3 = 
-  %_10 = 
-  %_12 = icmp slt i32 %_10, %_0
-  %_13 = zext i1 %_12 to i32
-  %_14 = icmp ne i32 %_13, 0
-  br i1 %_14, label %_while_body_6, label %_while_end_5
+$while_cond_4:
+  %$3 = 
+  %$10 = 
+  %$12 = icmp slt i32 %$10, %$0
+  %$13 = zext i1 %$12 to i32
+  %$14 = icmp ne i32 %$13, 0
+  br i1 %$14, label %$while_body_6, label %$while_end_5
 
-_while_body_6:
-  %_15 = add i32 %_3, %_4
-  %_9 = add i32 %_15, %_10
-  %_11 = add i32 %_10, 1
-  br label %_while_cond_4
+$while_body_6:
+  %$15 = add i32 %$3, %$4
+  %$9 = add i32 %$15, %$10
+  %$11 = add i32 %$10, 1
+  br label %$while_cond_4
 
-_while_end_5:
-  %_5 = add i32 %_4, 1
-  br label %_while_cond_2
+$while_end_5:
+  %$5 = add i32 %$4, 1
+  br label %$while_cond_2
 }
 "#
     );
